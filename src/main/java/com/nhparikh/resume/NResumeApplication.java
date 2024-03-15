5430283d8864ba99654622f607f29937ffda54b8
Nihar Parikh <niharparikh17@icloud.com>
2024-03-15 18:18:34 -0500

    Initial Commit

---
+++ src/main/java/com/nhparikh/resume/NResumeApplication.java
@@ -0,0 +1,13 @@
+package com.nhparikh.resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NResumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NResumeApplication.class, args);
+	}
+
+}

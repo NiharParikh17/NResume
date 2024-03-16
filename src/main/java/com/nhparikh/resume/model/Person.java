cb869d91e01205240e24a4e90dce57f394f6b9fd
Nihar Parikh <niharparikh17@icloud.com>
2024-03-15 19:50:56 -0500

    Renamed to person

---
+++ src/main/java/com/nhparikh/resume/model/Person.java
@@ -0,0 +1,45 @@
+package com.nhparikh.resume.model;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(name = "Person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Schema(name = "firstName", title = "First Name")
+    @Size(min = 1, message = "First name must be at least of size 1")
+    private String firstName;
+
+    @Schema(name = "lastName", title = "Last Name", nullable = true)
+    private String lastName;
+
+    @Schema(name = "middleName", title = "Middle Name", nullable = true)
+    private String middleName;
+
+    @Pattern(regexp = "", message = "Not a valid email")
+    @Schema(name = "email", title = "Email Address", example = "person.name@domain.com")
+    private String email;
+
+    @Pattern(regexp = "", message = "Not a valid phone number")
+    @Schema(name = "phone", title = "Phone Number", example = "1234567890")
+    private String phone;
+}
